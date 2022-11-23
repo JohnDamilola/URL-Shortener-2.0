7d6c417dfdb3ac456d003d134c1c66daf20ba950
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-22 21:07:22 -0500

    Update test_auth.py

--- backend/test/test_auth.py
+++ backend/test/test_auth.py
@@ -19,34 +19,14 @@ class AuthTestApp(unittest.TestCase):
         self.app.register_blueprint(auth_bp)
         self.app=self.app.test_client()
 
-    def test_register_post_route(self):
+    def test_register_route(self):
         '''Test the register route of our app'''
-        response=self.app.post('/register',json=dict(email='test_email@gmail.com',first_name='test_first',last_name='test_last',password='password123'))
+        response=self.app.post('/auth/register',json=dict(email='test_email@gmail.com',first_name='test_first',last_name='test_last',password='password123'))
         print(response.status_code)
         assert response.status_code==201
-    """
-    def test_index_post(self):
-        '''Test that the post request to the index route is not allowed'''
-        response=self.app.post('/')
-        assert response.status_code==405
-
-    def test_signup_route_registered_user(self):
-        '''Test the signup route of our app with a registered user'''
-        response=self.app.post('/signup',json=dict(email='aaronadb@gmail.com',password='flashcards123'))
-        assert response.status_code==400
-        
-    def test_signup_route_unregistered_user_invalid_email(self):
-        '''Test the signup route of our app with an unregistered user using an invalid email address'''
-        response=self.app.post('/signup',json=dict(email='test@gmail.com',password='password123'))
-        assert response.status_code==400
-        
-    def test_signup_route_registered_user(self):
-        '''Test the signup route of our app with a registered user'''
-        #This has been tested manually, programming this test is not possible right now, because the functionality for deleting an account has not been implemented as yet
-        pass
-        
-    def test_login_route_registered_user(self):
-        '''Test the login route of our app with an already registered user'''
+    """    
+    def test_login_route(self):
+        '''Test the login route of our app'''
         response=self.app.post('/login',json=dict(email='aaronadb@gmail.com',password='flashcards123'),follow_redirects=True)
         assert response.status_code==200
         
