78eb5b5ed12d46f1a9354a5f7d17cee8209bbb27
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-22 20:15:48 -0500

    Create test_app.py

---
+++ backend/test/test_app.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('backend/src')
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from backend.config import config
+from backend.extensions import db, bcrypt
+from backend.src.routes.auth import auth_bp
+from backend.src.models.user import User, login_manager
+import unittest
+
+class AuthTestApp(unittest.TestCase):
+    def setUp(self):
+        self.app=Flask(__name__, instance_relative_config=False)
+        self.app.config.from_object(config['development'])
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        register_blueprints(self.app)
+        register_extensions(self.app)
+        self.app=self.app.test_client()
+
+    def test_index_get_route(self):
+        '''Test the index route of our app'''
+        response=self.app.get('/')
+        assert response.status_code==200
+
+if __name__=="__main__":
+    unittest.main()
