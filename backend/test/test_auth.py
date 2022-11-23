bd8f53ab383dbea3437c55789fdbe923476d826a
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-22 21:11:49 -0500

    Update test_auth.py

--- backend/test/test_auth.py
+++ backend/test/test_auth.py
@@ -21,15 +21,16 @@ class AuthTestApp(unittest.TestCase):
 
     def test_register_route(self):
         '''Test the register route of our app'''
-        response=self.app.post('/auth/register',json=dict(email='aaronadb@gmail.com',first_name='test_first',last_name='test_last',password='password123'))
-        print(response.status_code)
+        response=self.app.post('/auth/register',json=dict(email='test_email@gmail.com',first_name='test_first',last_name='test_last',password='password123'))
+        #print(response.status_code)
         assert response.status_code==201
-    """    
+    
     def test_login_route(self):
         '''Test the login route of our app'''
-        response=self.app.post('/login',json=dict(email='aaronadb@gmail.com',password='flashcards123'),follow_redirects=True)
+        response=self.app.post('/auth/login',json=dict(email='test_email@gmail.com',password='password123'),follow_redirects=True)
+        print(response.message)
         assert response.status_code==200
-        
+    """    
     def test_login_route_wrong_password(self):
         '''Test the login route of our app with a registered user with a wrong password'''
         response=self.app.post('/login',json=dict(email='aaronadb@gmail.com',password='flashcards'))
