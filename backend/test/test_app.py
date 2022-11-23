7a311f241dbe617bcf796180d040125cf87ba052
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-22 20:20:13 -0500

    Update test_app.py

--- backend/test/test_app.py
+++ backend/test/test_app.py
@@ -15,8 +15,13 @@ class AuthTestApp(unittest.TestCase):
         self.app=Flask(__name__, instance_relative_config=False)
         self.app.config.from_object(config['development'])
         self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        register_blueprints(self.app)
-        register_extensions(self.app)
+        with self.app.app_context():
+            self.app.register_blueprint(auth_bp)
+            self.app.register_blueprint(shorten_links_bp)
+        with self.app.app_context():
+            db.init_app(self.app)
+            bcrypt.init_app(self.app)
+            login_manager.init_app(self.app)
         self.app=self.app.test_client()
 
     def test_index_get_route(self):
