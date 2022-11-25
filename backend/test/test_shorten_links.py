2dc2a5e42181edf403240792444f1162c9781696
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-25 15:28:10 -0500

    Create test_shorten_links.py

---
+++ backend/test/test_shorten_links.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('backend/src')
+import unittest
+from models.user import User
+from app import app
+
+class AuthTestApp(unittest.TestCase):
+    def setUp(self):
+        self.flask_app=app
+        self.app=self.flask_app.test_client()
+    
+    def test_link_route(self):
+      _=self.app.post('/auth/register',json=dict(email='test6@gmail.com',first_name='test6_first',last_name='test6_last',password='password6'))
+      with self.flask_app.app_context():
+            user=User.query.filter_by(email='test5@gmail.com').first()
+            id=user.id
+            response=self.app.get('/link/'+str(id))
+      assert response.status_code==200
+      
+      
+if __name__=="__main__":
+    unittest.main()
