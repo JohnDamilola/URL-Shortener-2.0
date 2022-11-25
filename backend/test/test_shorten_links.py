8430467679288c0f84b644afd491cb309562b5a7
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-25 17:04:55 -0500

    Update test_shorten_links.py

--- backend/test/test_shorten_links.py
+++ backend/test/test_shorten_links.py
@@ -15,7 +15,7 @@ class AuthTestApp(unittest.TestCase):
         with self.flask_app.app_context():
             user=User.query.filter_by(email='test6@gmail.com').first()
             uid=user.id
-            response=self.app.post('/link/create/',json=dict(id=,user_id=uid,long_url='https://google.com',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
+            response=self.app.post('/link/create/',json=dict(id='f74fa000-fc00-43c0-a0f0-9c0a27g00a0e',user_id=uid,long_url='https://google.com',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
         assert response.status_code==201
     
     def test_link_route(self):
