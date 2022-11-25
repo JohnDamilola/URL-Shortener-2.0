592deb6e85a5f3efa530cfa7eb8bd26ab72d689c
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-25 16:33:12 -0500

    Update test_shorten_links.py

--- backend/test/test_shorten_links.py
+++ backend/test/test_shorten_links.py
@@ -12,7 +12,7 @@ class AuthTestApp(unittest.TestCase):
     def test_link_route(self):
       _=self.app.post('/auth/register',json=dict(email='test6@gmail.com',first_name='test6_first',last_name='test6_last',password='password6'))
       with self.flask_app.app_context():
-            user=User.query.filter_by(email='test5@gmail.com').first()
+            user=User.query.filter_by(email='test6@gmail.com').first()
             id=user.id
             response=self.app.get('/link/'+str(id))
       assert response.status_code==200
