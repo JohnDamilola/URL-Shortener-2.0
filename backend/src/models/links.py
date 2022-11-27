3fd1c24c29c3fbb5f63706a72128d5f5bea7e0c6
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-27 18:39:15 -0500

    Resolve Merge conflicts link.py

--- backend/src/models/links.py
+++ backend/src/models/links.py
@@ -9,7 +9,7 @@ class Link(db.Model):
     __tablename__ = 'links'
  
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    stub = db.Column(db.String(10), unique=True, nullable=False)
+    stub = db.Column(db.String(100), unique=True, nullable=False)
     long_url = db.Column(db.String(2083), nullable=False)
     disabled = db.Column(db.Boolean, default=False, nullable=False)
     utm_source = db.Column(db.String(100), nullable=True)
