f9af1b4e575d78d4f6bf0ea87ef791006b9bc671
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-27 19:15:47 -0500

    Resolve Merge Conflicts app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -14,13 +14,13 @@ try:
     from .config import config
     from .extensions import db, bcrypt
     from .routes.auth import auth_bp
-    from .routes.shorten_links import shorten_links_bp
+    from .routes.links import links_bp
     from .models.user import User, login_manager
 except ImportError:
     from config import config
     from extensions import db, bcrypt
     from routes.auth import auth_bp
-    from routes.shorten_links import shorten_links_bp
+    from routes.links import links_bp
     from models.user import User, login_manager
 
 def create_app():
@@ -35,7 +35,7 @@ def create_app():
 def register_blueprints(app):
     with app.app_context():
         app.register_blueprint(auth_bp)
-        app.register_blueprint(shorten_links_bp)
+        app.register_blueprint(links_bp)
     return None
 
 def register_extensions(app):
