dbaf49645777c96f0ed1fd848e0fc9adbdcd1036
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-30 16:53:40 -0500

    Update test_shorten_links.py

--- backend/test/test_shorten_links.py
+++ backend/test/test_shorten_links.py
@@ -25,7 +25,6 @@ class AuthTestApp(unittest.TestCase):
         _=self.app.post('/auth/register',json=dict(email='test7@gmail.com',first_name='test7_first',last_name='test7_last',password='password7'))
         with self.flask_app.app_context():
             user=User.query.filter_by(email='test7@gmail.com').first()
-            id=user.id
             uid=user.id
             link_id=uuid.uuid4()
             _=self.app.post('/link/create',json=dict(id=link_id,user_id=uid,long_url='https://yahoo.in',title='Yahoo',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
@@ -37,12 +36,11 @@ class AuthTestApp(unittest.TestCase):
         _=self.app.post('/auth/register',json=dict(email='test8@gmail.com',first_name='test8_first',last_name='test8_last',password='password8'))
         with self.flask_app.app_context():
             user=User.query.filter_by(email='test8@gmail.com').first()
-            id=user.id
             uid=user.id
             _=self.app.post('/auth/login',json=dict(email='test8@gmail.com',password='password8'))
             _=self.app.post('/link/create',json=dict(id=uuid.uuid4(),user_id=uid,long_url='https://google.in',title='Google2',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
             _=self.app.post('/link/create',json=dict(id=uuid.uuid4(),user_id=uid,long_url='https://yahoo.com',title='Yahoo2',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
-            response=self.app.get('/link/all')
+            response=self.app.get('/link/all',query_string=dict(localId=uid))
             print(response.status_code)
         assert response.status_code==200
      
