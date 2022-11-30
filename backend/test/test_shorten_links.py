169e1b993389a69231a32f8f0bd4a2f4a8c5210b
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-30 16:05:43 -0500

    Update test_shorten_links.py

--- backend/test/test_shorten_links.py
+++ backend/test/test_shorten_links.py
@@ -17,18 +17,20 @@ class AuthTestApp(unittest.TestCase):
             user=User.query.filter_by(email='test6@gmail.com').first()
             uid=user.id
             response=self.app.post('/link/create',json=dict(id=uuid.uuid4(),user_id=uid,long_url='https://google.com',title='Google',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
-        print("Create 201",response.status_code)
         assert response.status_code==201
-    """
+    
     def test_link_route(self):
       _=self.app.post('/auth/register',json=dict(email='test7@gmail.com',first_name='test7_first',last_name='test7_last',password='password7'))
       with self.flask_app.app_context():
             user=User.query.filter_by(email='test7@gmail.com').first()
             id=user.id
-            response=self.app.get('/link/'+str(id))
+            uid=user.id
+            link_id=uuid.uuid4()
+            _=self.app.post('/link/create',json=dict(id=link_id,user_id=uid,long_url='https://google.com',title='Google',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
+            response=self.app.get('/link/'+str(link_id))
       print(response.status_code)
       assert response.status_code==200
-      """
+     
       
       
 if __name__=="__main__":
