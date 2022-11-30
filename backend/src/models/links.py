70cd2acbdb15561f0ddbc6981e9e8c4d794a8c68
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-30 15:22:56 -0500

    Update links.py

--- backend/src/models/links.py
+++ backend/src/models/links.py
@@ -25,8 +25,9 @@ class Link(db.Model):
 	# make a relationship with 'User' model
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'))
 
-    def __init__(self, user_id, stub, long_url, title, disabled, utm_source, utm_medium, utm_campaign, utm_term, utm_content, password_hash, expire_on):
-        self.user_id=user_id
+    def __init__(self, id, user_id, stub, long_url, title, disabled, utm_source, utm_medium, utm_campaign, utm_term, utm_content, password_hash, expire_on):
+        self.id=id
+	self.user_id=user_id
         self.stub=stub
         self.long_url = long_url
         self.title = title
