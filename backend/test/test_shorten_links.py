40d1be7eec4225f6d52cd828081bfd7c45fb155f
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-30 16:16:45 -0500

    Update test_shorten_links.py

--- backend/test/test_shorten_links.py
+++ backend/test/test_shorten_links.py
@@ -26,7 +26,7 @@ class AuthTestApp(unittest.TestCase):
             id=user.id
             uid=user.id
             link_id=uuid.uuid4()
-            _=self.app.post('/link/create',json=dict(id=link_id,user_id=uid,long_url='https://google.com',title='Google',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
+            _=self.app.post('/link/create',json=dict(id=link_id,user_id=uid,long_url='https://yahoo.in',title='Yahoo',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
             response=self.app.get('/link/'+str(link_id))
       print(response.status_code)
       assert response.status_code==200
