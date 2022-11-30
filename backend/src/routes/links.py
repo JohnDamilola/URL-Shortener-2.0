7fd37cc256e8c658bbfaf3eba400a0c2afd14546
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-30 17:21:38 -0500

    Update links.py

--- backend/src/routes/links.py
+++ backend/src/routes/links.py
@@ -133,6 +133,7 @@ def update(id):
             status = 201
         ), 201
     except Exception as e:
+        print(e)
         return jsonify(
             message = f'Update Link Failed {e}',
             status = 400
