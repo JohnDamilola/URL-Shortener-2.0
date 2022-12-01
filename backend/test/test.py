930e7af7b23daa2368bbb43be21daf83b48d7a2e
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-11-30 23:50:50 -0500

    Update test.py

--- backend/test/test.py
+++ backend/test/test.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('backend/test')
 from test_auth import AuthTestApp
-from test_shorten_links import LinkTestApp
+from test_link import LinkTestApp
 import unittest
 
 if __name__=="__main__":
