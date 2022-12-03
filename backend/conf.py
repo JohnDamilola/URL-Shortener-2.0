81bc07103cef91f0318dcdfc8d8df3f04c3d8230
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-12-02 22:02:55 -0500

    Create conf.py

---
+++ backend/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'URL Shortener 2.0'
+copyright = '2022, John Damilola, Leo Hsiang, Swarangi Gaurkar, Kritka Javali, Aaron Dias Barreto'
+author = 'John Damilola, Leo Hsiang, Swarangi Gaurkar, Kritika Javali, Aaron Dias Barreto'
+
+version = '1'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import sys, os
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(1, os.path.abspath('Documentation'))
+
+extensions = ['sphinx.ext.autodoc']
+
+#templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+#html_static_path = ['_static']
