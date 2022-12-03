9f20bde431f271e9d59689a1f8068c453cf930ef
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-12-02 21:54:20 -0500

    Create auth.py

---
+++ backend/Documentation/auth.py
@@ -0,0 +1,138 @@
+import datetime
+from flask import Blueprint, jsonify               #import dependancies
+from flask import current_app as app
+from flask_login import login_required, login_user, logout_user
+from flask_cors import cross_origin
+from flask import request
+try:
+    from ..models.user import User, db, load_user
+    from ..extensions import bcrypt
+except ImportError:
+    from models.user import User, db, load_user
+    from extensions import bcrypt
+import jwt
+
+auth_bp = Blueprint(
+    'auth_bp', __name__
+)
+
+@auth_bp.route('/auth/register', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def register():                                       #this method is used to create new users and register them in firebase
+    try:
+        data = request.get_json()
+        email = data['email']
+        first_name = data['first_name']
+        last_name = data['last_name']
+        password = data['password']
+        
+        
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(email=email, first_name=first_name, last_name=last_name, password_hash=password_hash)
+        db.session.add(new_user)
+        db.session.commit()
+    
+        return jsonify(
+            user = new_user.to_json(),                            
+            message = 'Registered Successfully',    #if the registration process is successful, this message is displayed
+            status = 201
+        ), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(
+            message = f'Registration Failed {e}',        #if the registration process is not successful, this message is displayed
+            status = 400
+        ), 400
+
+@auth_bp.route('/auth/login', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def login():                                        #this method is used by registered users to sign in to their account
+    try:
+        data = request.get_json()
+        email = data['email']
+        password = data['password']
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if user:
+            if bcrypt.check_password_hash(user.password_hash, password):
+                token = jwt.encode({'public_id': str(user.id), 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=12)}, app.config['SECRET_KEY'])
+                login_user(user)
+                return jsonify(
+                    user = user.to_json(),
+                    token = token,
+                    message = 'Login Successful',           #if login is successful, this message is displayed
+                    status = 200
+                ), 200
+            else:
+                return jsonify(
+                    message = 'Invalid Password',           #if login is successful, this message is displayed
+                    status = 400
+                ), 400
+        else:
+            return jsonify(
+                message = 'User with this account not found',           #if login is successful, this message is displayed
+                status = 400
+            ), 400
+    except Exception as e:
+        return jsonify(
+            message = f"An error occurred {e}",
+            # message = 'An error occurred, please try again',               #if login is not successful, this message is displayed
+            status = 500
+        ), 500
+    
+@auth_bp.route('/user/update/<id>', methods=['PATCH'])
+@cross_origin(supports_credentials=True)   
+def update(id):
+    '''This method is called when the user requests to update the their credentials.'''
+    try:
+        data = request.get_json()
+        first_name = data['first_name']
+        last_name = data['last_name']
+        email = data['email']
+        
+        user = load_user(id)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        # user = User.query.filter_by(id=id).first()
+        # db.session.update(first_name=first_name, last_name=last_name)
+        db.session.commit()
+
+        return jsonify(
+            user = user.to_json(),
+            message = 'Update User Successful',
+            status = 201
+        ), 201
+    except Exception as e:
+        return jsonify(
+            message = f'Update User Failed {e}',
+            status = 400
+        ), 400
+
+@auth_bp.route('/user/delete/<id>', methods = ['DELETE'])
+@cross_origin(supports_credentials=True)
+def delete(id):
+    '''This method is called when the user requests to delete the their account. Only the link id is required to delete the deck.'''
+    try:
+        db.session.query(User).filter_by(id=id).delete()
+        db.session.commit()
+        return jsonify(
+            message = 'Delete User Successful',
+            status = 200
+        ), 200
+    except Exception as e:
+        return jsonify(
+            message = f'Delete User Failed {e}',
+            status = 400
+        ), 400    
+   
+@auth_bp.route('/auth/logout')
+@login_required
+@cross_origin(supports_credentials=True)
+def logout():
+    logout_user()
+      
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
