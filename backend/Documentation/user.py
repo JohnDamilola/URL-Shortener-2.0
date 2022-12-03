ae4ab1260e1ee1c2e8f630dcfee90274a03083a6
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-12-02 21:55:48 -0500

    Create user.py

---
+++ backend/Documentation/user.py
@@ -0,0 +1,46 @@
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin
+try:
+ from ..extensions import db
+except ImportError:
+ from extensions import db
+from sqlalchemy.dialects.postgresql import UUID
+
+login_manager = LoginManager()
+
+ 
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+ 
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = db.Column(db.String(80), unique=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    password_hash = db.Column(db.String())
+    created_on = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), nullable=False)
+    updated_on = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), nullable=False, server_onupdate=db.func.now())
+ 
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+     
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+    
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def to_json(self):
+        return {
+            'id': str(self.id),
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'created_on': self.created_on,
+            'updated_on': self.updated_on
+        }
+ 
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
