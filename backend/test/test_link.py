0f31526272d66f5a0b1d41b6f830d487c4b62237
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-12-04 14:32:22 -0500

    Update test_link.py

--- backend/test/test_link.py
+++ backend/test/test_link.py
@@ -31,8 +31,7 @@ class LinkTestApp(unittest.TestCase):
             user=User.query.filter_by(email='test7@gmail.com').first()
             uid=user.id
             link_id=uuid.uuid4()
-            _=self.app.post('/links/create',json=dict(id=link_id,user_id=uid,long_url='https://yahoo.in',title='Yahoo',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
-            print(_.status_code)
+            _=self.app.post('/links/create',query_string=dict(user_id=uid),json=dict(id=link_id,user_id=uid,long_url='https://yahoo.in',title='Yahoo',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
             response=self.app.get('/links/'+str(link_id))
         print("link",response.status_code)
         assert response.status_code==200
@@ -44,8 +43,8 @@ class LinkTestApp(unittest.TestCase):
             user=User.query.filter_by(email='test8@gmail.com').first()
             uid=user.id
             self.app.post('/auth/login',json=dict(email='test8@gmail.com',password='password8'))
-            self.app.post('/links/create',json=dict(id=uuid.uuid4(),user_id=uid,long_url='https://google.in',title='Google2',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
-            self.app.post('/links/create',json=dict(id=uuid.uuid4(),user_id=uid,long_url='https://yahoo.com',title='Yahoo2',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
+            self.app.post('/links/create',query_string=dict(user_id=uid),json=dict(id=uuid.uuid4(),user_id=uid,long_url='https://google.in',title='Google2',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
+            self.app.post('/links/create',query_string=dict(user_id=uid),json=dict(id=uuid.uuid4(),user_id=uid,long_url='https://yahoo.com',title='Yahoo2',disabled=False,utm_source='test6_source',utm_medium='test6_medium',utm_campaign='test6_campaign',utm_term='test6_term',utm_content='test6_content',password_hash='link_password',expire_on=datetime.datetime(2022,11,25)))
             response=self.app.get('/links/all',query_string=dict(localId=uid))
         print("all",response.status_code)
         assert response.status_code==200
